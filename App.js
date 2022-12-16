6517cb7c37273329e10ce9f11fcb57b6b48ac6d3
marc.torsoc <marc.torsoc@gmail.com>
2022-12-16 21:29:24 +0100

    initial version

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { enableScreens } from "react-native-screens";
+import store, { persistor } from "./src/redux/store";
+import Navigation from "./src/navigation";
+import SceneAppLoading from "./src/scenes/auth/sceneAppLoading";
+
+enableScreens();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={<SceneAppLoading />} persistor={persistor}>
+        <StatusBar hidden />
+        <Navigation />
+      </PersistGate>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
