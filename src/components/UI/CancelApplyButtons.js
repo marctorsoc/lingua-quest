d664b0090a988550ae8602bb7cb7f12b5d34adad
marc.torsoc <marc.torsoc@gmail.com>
2024-10-28 19:33:13 +0100

    add cancelApplyButtons file

---
+++ src/components/UI/CancelApplyButtons.js
@@ -0,0 +1,63 @@
+import { StyleSheet, Text, View } from "react-native";
+import { GlobalStyles, ScreensStyles } from "../../constants/styles";
+import { useTranslation } from "react-i18next";
+import Button from "./Button";
+
+export function CancelApplyButtons({
+  onCancel,
+  onApply,
+  applyButtonLabel,
+}) {
+  const { t } = useTranslation();
+
+  return (
+    // two buttons side by side to Cancel or Apply
+    <View style={styles.buttons}>
+      <Button
+        style={[
+          ScreensStyles.button,
+          styles.button,
+          styles.cancelButton,
+        ]}
+        onPress={onCancel}
+      >
+        <Text style={ScreensStyles.buttonLabel}>
+          {t("GLOBAL.CANCEL")}
+        </Text>
+      </Button>
+      <Button
+        style={[
+          ScreensStyles.button,
+          styles.button,
+          styles.applyButton,
+        ]}
+        onPress={onApply}
+      >
+        <Text style={ScreensStyles.buttonLabel}>
+          {applyButtonLabel}
+        </Text>
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttons: {
+    marginTop: "10%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: "3%",
+  },
+  // cancelButton: {
+  //   // backgroundColor: "#5894f2",
+  //   backgroundColor: GlobalStyles.colors.primaryButton,
+  // },
+  // applyButton: {
+  //   // backgroundColor: "#f7b95b",
+  //   backgroundColor: GlobalStyles.colors.primary500,
+  // },
+});
